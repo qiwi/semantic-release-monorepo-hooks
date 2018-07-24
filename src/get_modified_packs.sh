163186c8b497b4e464cdb0104a52a77e1275a8a8
Anton Golub <mailbox@antongolub.ru>
2018-07-24 18:12:30 +0300

    fix(get_modified_packs): should run from any dir

--- src/get_modified_packs.sh
+++ src/get_modified_packs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo $(jq '.name' $(git diff HEAD $(git rev-list -n 1 $(git describe --abbrev=0 --tags)) --name-only | grep packages | sed -E 's/^([^/]+\/[^/]+)\/.*$/\1\/package.json/' | sort | uniq))
+root=$(git rev-parse --show-toplevel | sed 's/\//\\\//g')
+sedarg="s/^([^/]+\/[^/]+)\/.*$/"$root"\/\1\/package.json/"
+
+echo $(jq '.name' $(git diff HEAD $(git rev-list -n 1 $(git describe --abbrev=0 --tags)) --name-only | grep packages | sed -E ${sedarg} | sort | uniq))
