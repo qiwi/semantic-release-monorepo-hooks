1da5121ed07a2f7cdca064daf4f7f307e6c555cf
Anton Golub <mailbox@antongolub.ru>
2018-08-26 10:43:01 +0300

    fix(sh): add quotes for git tag message arg

--- src/sh/add_tag.sh
+++ src/sh/add_tag.sh
@@ -2,7 +2,7 @@
 
 sh $( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"/git_prepare_remote.sh"
 
-tag=$(git tag -a $1 -m $2)
+tag=$(git tag -a $1 -m "$2")
 git push foobar --tags
 
 echo tag
