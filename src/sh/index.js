9ed53c998e7b75ca296d43f4cfa4b067a35a791a
Anton Golub <mailbox@antongolub.ru>
2018-08-26 11:34:37 +0300

    fix(git): add quotes to passed args

--- src/sh/index.js
+++ src/sh/index.js
@@ -2,7 +2,7 @@ const path = require('../path')
 const cp = require('child_process')
 
 module.exports = {
-  addTag (tag, message) { return cp.execSync(`sh ${path.ADD_TAG_SH} ${tag} ${message}`).toString() },
+  addTag (tag, message) { return cp.execSync(`sh ${path.ADD_TAG_SH} "${tag}" "${message}"`).toString() },
   getLastTag () { return cp.execSync(`sh ${path.GET_LAST_TAG_SH}`).toString() },
   getLastTagMessage () { return cp.execSync(`sh ${path.LAST_TAG_MESSAGE_SH}`).toString() },
   dropLastTag () { return cp.execSync(`sh ${path.DROP_TAG_SH}`).toString() },
