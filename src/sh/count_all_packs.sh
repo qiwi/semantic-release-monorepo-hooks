3abcca7c54695b7e4634474df7414559cec1a861
Anton Golub <golub.anton@gmail.com>
2019-05-28 13:01:42 +0300

    fix(count_all_packs): add missed $ and remove redundant reassgnment

--- src/sh/count_all_packs.sh
+++ src/sh/count_all_packs.sh
@@ -2,4 +2,4 @@
 
 value=$(git diff HEAD $(git rev-list --max-parents=0 HEAD) --name-only | grep packages | sed -E 's/^[^/]+\/([^/]+)\/.*$/\1/' | sort | uniq | wc -l)
 
-echo echo $((value + 0))
+echo $((value + 0))
