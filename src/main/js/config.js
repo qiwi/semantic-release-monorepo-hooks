7f9c40b4d27119d832f28efcc0e979b2956980ce
Anton Golub <antongolub@antongolub.com>
2022-05-24 14:53:01 +0300

    feat: use cosmiconfig for release config loading

--- src/main/js/config.js
+++ src/main/js/config.js
@@ -1,25 +1,19 @@
 const path = require('path')
+const { cosmiconfigSync } = require('cosmiconfig')
 const { gitRootSync } = require('@antongolub/git-root')
-const log = require('./log')
 
 const ROOT = path.resolve(gitRootSync())
-const RELEASERC = '.releaserc.js'
-const PACKAGEJSON = 'package.json'
+// Copied from get-config.js in semantic-release
+const CONFIG_NAME = 'release'
+const CONFIG_FILES = [
+  'package.json',
+  `.${CONFIG_NAME}rc`,
+  `.${CONFIG_NAME}rc.json`,
+  `.${CONFIG_NAME}rc.yaml`,
+  `.${CONFIG_NAME}rc.yml`,
+  `.${CONFIG_NAME}rc.js`,
+  `.${CONFIG_NAME}rc.cjs`,
+  `${CONFIG_NAME}.config.js`
+]
 
-const releaserc = (() => {
-  const releasercPath = path.resolve(ROOT, RELEASERC)
-  const packageJsonPath = path.resolve(ROOT, PACKAGEJSON)
-
-  try {
-    return require(releasercPath)
-  } catch (err) {
-    log(`'${RELEASERC}' is not found, so '${PACKAGEJSON}#release' will be used instead`)
-    log('err=', err)
-
-    const pkg = require(packageJsonPath)
-
-    return pkg.release || pkg
-  }
-})()
-
-module.exports = releaserc
+module.exports = cosmiconfigSync(CONFIG_NAME, { searchPlaces: CONFIG_FILES }).search(ROOT).config
--- src/test/js/config.js
+++ src/test/js/config.js
@@ -1,8 +1,9 @@
 const config = require('../../main/js/config')
-const readPkg = require('read-pkg').sync
 
 describe('config', () => {
   it('returns `release` config', () => {
-    expect(config).toEqual(readPkg().release)
+    expect(config).toMatchObject({
+      branches: ['master']
+    })
   })
 })
